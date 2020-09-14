f8743f7031a865daabc08af15ad4c5e4b38e8075
H Paterson <26947190+patha454@users.noreply.github.com>
2020-09-14 17:41:49 +1200

    Feature: Setup clang-tidy.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,10 @@
 #include "ghost/asm/linux.h"
 
+#define TEST_WRITE_LEN 11
+#define TEST_EXIT_CODE 69
+
 void __start(void)
 {
-    write(1, "test print\0", 30);
-    exit_group(15);
+    write(1, "test print\n", TEST_WRITE_LEN);
+    exit_group(TEST_EXIT_CODE);
 }
