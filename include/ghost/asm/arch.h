f65c22b523306f145be4b9649da6e7831e6c61d7
H Paterson <26947190+patha454@users.noreply.github.com>
2020-09-16 21:04:48 +1200

    Documentation: Document `get_stack_pointer`.

--- include/ghost/asm/arch.h
+++ include/ghost/asm/arch.h
@@ -7,4 +7,15 @@
  * @copyright BSL-1.0.
  */
 
-extern void* get_stack_pointer();
+/**
+ * Get the stack pointer of the program *before* `get_stack_pointer` was
+ * called.
+ *
+ * `get_stack_pointer` is intended for getting the stack frame provided to the
+ * loader by the syster - often containing information passed from the kernel.
+ *
+ * @note Under most ABIs, calling `get_stack_pointer` would change the stack
+ * pointer into the function's own frame, so these implementations will return
+ * the base pointer - which is the caller's stack pointer.
+ */
+extern intptr_t get_stack_pointer();
